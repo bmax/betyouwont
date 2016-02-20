74053600f7729ea8e3c77e8692468f9b2534250c
Brandon Max <bmax1337@gmail.com>
2016-02-20 13:59:29 -0500

    Fix merge

--- Components/Header.js
+++ Components/Header.js
@@ -4,21 +4,6 @@ export default React.createClass({
   render() {
    return (
     <header>
-<<<<<<< HEAD
-      <video autoPlay mute id="background">
-      <source src="dare.mp4" type="video/mp4" />
-      </video>
-          <div className="container">
-              <div className="intro-text">
-                  <div className="intro-lead-in">Bet You Wont!</div>
-                  <div className="intro-heading">Would you do it?</div>
-                  <a href="#bets" className="page-scroll btn btn-xl">Tell Me More</a>
-              </div>
-          </div>
-    </header>
-    )
-  }
-=======
 <video autoPlay muted id="background">
 <source src="dare.mp4" type="video/mp4" />
 </video>
@@ -32,5 +17,4 @@ export default React.createClass({
 </header>
 )
 }
->>>>>>> cdb3c7d959211ebf745206ee274580f23a44e3c9
 });
